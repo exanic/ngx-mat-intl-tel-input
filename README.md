6e436195633d7c51069a1f3614c0c30b0e4e11c6
Marco <75484481+polojs@users.noreply.github.com>
2024-09-30 11:03:34 +0200

    feat(teammsmt-2280): improve existing mask (seperate country code dropdown) (#2)
    * test
    
    * remove unused classes
    
    * adds formatting
    
    * remove unused mat menu
    
    * updated changelog
    
    * changes
    
    * adapt for exanic

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 # International Telephone Input for Angular Material (NgxMatIntlTelInput)
 
-An Angular Material package for entering and validating international telephone numbers. It adds a flag dropdown to any input, detects the user's country, displays a relevant placeholder and provides formatting/validation methods.
+An Angular Material package for entering and validating international telephone numbers. It adds a flag dropdown to any input, detects the user's country, displays a relevant placeholder and provides formatting/validation methods. Adapted for exanic to seperate country code selection from phone input.
 
 **Supports:**
 
-- Angular 14
-- Angular Material 14
+- Angular 15
+- Angular Material 15
 - ReactiveFormsModule
 - FormsModule
 - Validation with [libphonenumber-js](https://github.com/catamphetamine/libphonenumber-js)
+- select search with [ngx-mat-select-search](https://www.npmjs.com/package/ngx-mat-select-search/v/7.0.7)
 
 ## Installation
 
 ### Install Dependencies
 
 `$ npm install libphonenumber-js --save`
+`$ npm install ngx-mat-select-search`
 
 ### Install This Library
 
@@ -30,20 +32,21 @@ Add `NgxMatIntlTelInputComponent` to your component file:
 imports: [NgxMatIntlTelInputComponent];
 ```
 
+If close icon is not displayed correctly on ngx mat select search, try adding:
+
+<link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"/>
+
+to your `index.html`
+
 ## Example
 
 Refer to main app in this repository for working example.
 
 ```html
 <form #f="ngForm" [formGroup]="phoneForm">
-  <ngx-mat-intl-tel-input
-    [preferredCountries]="['us', 'gb']"
-    [enablePlaceholder]="true"
-    [enableSearch]="true"
-    name="phone"
-    describedBy="phoneInput"
-    formControlName="phone"
-  ></ngx-mat-intl-tel-input>
+  <ngx-mat-intl-tel-input [preferredCountries]="['us', 'gb']" [enablePlaceholder]="true" [enableSearch]="true" name="phone" describedBy="phoneInput" formControlName="phone"></ngx-mat-intl-tel-input>
 </form>
 ```
 
@@ -51,7 +54,6 @@ Refer to main app in this repository for working example.
 
 <form #f="ngForm" [formGroup]="phoneForm">
   <ngx-mat-intl-tel-input
-  [preferredCountries]="['us', 'gb']"
   [enablePlaceholder]="true"
   [enableSearch]="true"
   name="phone"
@@ -61,39 +63,28 @@ Refer to main app in this repository for working example.
 
 ```
 
-If you want to show the sample number for the country selected or errors , use mat-hint anf mat-error as
+If you want to show the sample number for the country selected or errors , use mat-hint and mat-error as
 
 ```html
 <form #f="ngForm" [formGroup]="phoneForm">
-  <ngx-mat-intl-tel-input
-    [preferredCountries]="['us', 'gb']"
-    [onlyCountries]="['us', 'gb', 'es']"
-    [enablePlaceholder]="true"
-    name="phone"
-    formControlName="phone"
-    #phone
-  ></ngx-mat-intl-tel-input>
+  <ngx-mat-intl-tel-input [preferredCountries]="['us', 'gb']" [onlyCountries]="['us', 'gb', 'es']" [enablePlaceholder]="true" name="phone" formControlName="phone" #phone></ngx-mat-intl-tel-input>
   <mat-hint>e.g. {{phone.selectedCountry.placeHolder}}</mat-hint>
-  <mat-error *ngIf="f.form.controls['phone']?.errors?.required"
-    >Required Field</mat-error
-  >
-  <mat-error *ngIf="f.form.controls['phone']?.errors?.validatePhoneNumber"
-    >Invalid Number</mat-error
-  >
+  <mat-error *ngIf="f.form.controls['phone']?.errors?.required">Required Field</mat-error>
+  <mat-error *ngIf="f.form.controls['phone']?.errors?.validatePhoneNumber">Invalid Number</mat-error>
 </form>
 ```
 
 ## Options
 
-| Options            | Type       | Default     | Description                                                                         |
-| ------------------ | ---------- | ----------- | ----------------------------------------------------------------------------------- | --- |
-| preferredCountries | `string[]` | `[]`        | List of country abbreviations, which will appear at the top.                        |
-| onlyCountries      | `string[]` | `[]`        | List of manually selected country abbreviations, which will appear in the dropdown. |     |
-| inputPlaceholder   | `string`   | `undefined` | Placeholder for the input component.                                                |
-| enablePlaceholder  | `boolean`  | `true`      | Input placeholder text, which adapts to the country selected.                       |
-| enableSearch       | `boolean`  | `false`     | Whether to display a search bar to help filter down the list of countries           |
-| format             | `string`   | `default`   | Format of "as you type" input. Possible values: national, international, default    |
-| describedBy        | `string`   | `undefined` | Use aria-described by with the input field                                          |
+| Options           | Type       | Default     | Description                                                                         |
+| ----------------- | ---------- | ----------- | ----------------------------------------------------------------------------------- |
+| onlyCountries     | `string[]` | `[]`        | List of manually selected country abbreviations, which will appear in the dropdown. |
+| inputPlaceholder  | `string`   | `undefined` | Placeholder for the input component.                                                |
+| enablePlaceholder | `boolean`  | `true`      | Input placeholder text, which adapts to the country selected.                       |
+| enableSearch      | `boolean`  | `false`     | Whether to display a search bar to help filter down the list of countries           |
+| format            | `string`   | `default`   | Format of "as you type" input. Possible values: national, international, default    |
+| describedBy       | `string`   | `undefined` | Use aria-described by with the input field                                          |
+| searchPlaceholder | `string`   | `undefined` | Search label for country select search dropdown                                     |
 
 ## Library Contributions
 
@@ -103,17 +94,13 @@ If you want to show the sample number for the country selected or errors , use m
 - Update README.md
 - Pull request.
 
-### Helpful commands
+### How to publish
 
-- Build lib: `$ npm run build_lib`
-- Copy license and readme files: `$ npm run copy-files`
-- Create package: `$ npm run npm_pack`
-- Build lib and create package: `$ npm run package`
+- increase version number in: `projects/ngx-mat-intl-tel-input/package.json`
+- Build lib and create package from root: `$ npm run package`
+- Copy license and readme files from root: `$ npm run copy-files` (use git bash if cp error)
+- cd into `dist/ngx-mat-intl-tel-input & run npm publish`
 
 ### Use locally
 
-After building and creating package, you can use it locally too.
-
-In your project run:
-
-`$ npm install --save {{path to your local '*.tgz' package file}}`
+Running `npm run start` in root allows you to test changes made to the library.
--- projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.component.ts
+++ projects/ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.component.ts
@@ -13,8 +13,8 @@ import {
   Output,
   Self,
   ViewChild,
-} from '@angular/core';
-import {MatFormFieldControl} from '@angular/material/form-field';
+} from "@angular/core";
+import { MatFormFieldControl } from "@angular/material/form-field";
 
 import {
   FormGroupDirective,
@@ -23,7 +23,8 @@ import {
   NgForm,
   NG_VALIDATORS,
   ReactiveFormsModule,
-} from '@angular/forms';
+  FormControl,
+} from "@angular/forms";
 import {
   AsYouType,
   CountryCode as CC,
@@ -32,28 +33,26 @@ import {
   NationalNumber,
   parsePhoneNumberFromString,
   PhoneNumber,
-} from 'libphonenumber-js';
-import {CountryCode, Examples} from './data/country-code';
-import {Country} from './model/country.model';
-import {PhoneNumberFormat} from './model/phone-number-format.model';
-import {phoneNumberValidator} from './ngx-mat-intl-tel-input.validator';
-
-import {FocusMonitor} from '@angular/cdk/a11y';
-import {coerceBooleanProperty} from '@angular/cdk/coercion';
-import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+} from "libphonenumber-js";
+import { CountryCode, Examples } from "./data/country-code";
+import { Country } from "./model/country.model";
+import { PhoneNumberFormat } from "./model/phone-number-format.model";
+import { phoneNumberValidator } from "./ngx-mat-intl-tel-input.validator";
+import { FocusMonitor } from "@angular/cdk/a11y";
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
 import {
   CanUpdateErrorState,
   ErrorStateMatcher,
   mixinErrorState,
   _AbstractConstructor,
   _Constructor,
-} from '@angular/material/core';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatInput, MatInputModule} from '@angular/material/input';
-import {MatMenu, MatMenuModule} from '@angular/material/menu';
-import {Subject} from 'rxjs';
-import {SearchPipe} from './search.pipe';
+} from "@angular/material/core";
+import { MatInput, MatInputModule } from "@angular/material/input";
+import { Subject } from "rxjs";
+import { NgxMatSelectSearchModule } from "ngx-mat-select-search";
+import { MatSelectModule } from "@angular/material/select";
 
 class NgxMatIntlTelInputBase {
   readonly stateChanges = new Subject<void>();
@@ -64,8 +63,7 @@ class NgxMatIntlTelInputBase {
     public _parentFormGroup: FormGroupDirective,
     /** @docs-private */
     public ngControl: NgControl
-  ) {
-  }
+  ) {}
 }
 
 declare type CanUpdateErrorStateCtor = _Constructor<CanUpdateErrorState> &
@@ -76,23 +74,22 @@ const _NgxMatIntlTelInputMixinBase: CanUpdateErrorStateCtor &
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'ngx-mat-intl-tel-input',
-  templateUrl: './ngx-mat-intl-tel-input.component.html',
-  styleUrls: ['./ngx-mat-intl-tel-input.component.scss'],
+  selector: "ngx-mat-intl-tel-input",
+  templateUrl: "./ngx-mat-intl-tel-input.component.html",
+  styleUrls: ["./ngx-mat-intl-tel-input.component.scss"],
   standalone: true,
   imports: [
     CommonModule,
     FormsModule,
+    MatSelectModule,
     MatInputModule,
-    MatMenuModule,
     MatButtonModule,
-    MatDividerModule,
+    NgxMatSelectSearchModule,
     ReactiveFormsModule,
-    SearchPipe
   ],
   providers: [
     CountryCode,
-    {provide: MatFormFieldControl, useExisting: NgxMatIntlTelInputComponent},
+    { provide: MatFormFieldControl, useExisting: NgxMatIntlTelInputComponent },
     {
       provide: NG_VALIDATORS,
       useValue: phoneNumberValidator,
@@ -103,11 +100,13 @@ const _NgxMatIntlTelInputMixinBase: CanUpdateErrorStateCtor &
 })
 export class NgxMatIntlTelInputComponent
   extends _NgxMatIntlTelInputMixinBase
-  implements OnInit,
+  implements
+    OnInit,
     OnDestroy,
     DoCheck,
     CanUpdateErrorState,
-    MatFormFieldControl<any> {
+    MatFormFieldControl<any>
+{
   static nextId = 0;
 
   @Input() preferredCountries: Array<string> = [];
@@ -119,7 +118,8 @@ export class NgxMatIntlTelInputComponent
   @Input() errorStateMatcher: ErrorStateMatcher = new ErrorStateMatcher();
   @Input() enableSearch = false;
   @Input() searchPlaceholder: string | undefined;
-  @Input() describedBy = '';
+  @Input() describedBy = "";
+  @Input() phoneLabel = "Number";
 
   @Input()
   get format(): PhoneNumberFormat {
@@ -132,7 +132,6 @@ export class NgxMatIntlTelInputComponent
     this.stateChanges.next(undefined);
   }
 
-  @ViewChild(MatMenu) matMenu: MatMenu | undefined;
   private _placeholder: string | undefined;
   private _required = false;
   private _disabled = false;
@@ -140,9 +139,8 @@ export class NgxMatIntlTelInputComponent
   focused = false;
   @HostBinding()
   id = `ngx-mat-intl-tel-input-${NgxMatIntlTelInputComponent.nextId++}`;
-  phoneNumber: NationalNumber | undefined;
+  phoneNumber: NationalNumber | string | undefined;
   allCountries: Array<Country> = [];
-  preferredCountriesInDropDown: Array<Country> = [];
   selectedCountry: Country | undefined;
   numberInstance: PhoneNumber | undefined;
   value: E164Number | string | undefined;
@@ -150,18 +148,16 @@ export class NgxMatIntlTelInputComponent
   @Output() countryChanged = new EventEmitter<Country>();
 
   private previousFormattedNumber: string | undefined;
-  private _format: PhoneNumberFormat = 'default';
+  private _format: PhoneNumberFormat = "default";
 
   static getPhoneNumberPlaceHolder(countryISOCode: CC): string | undefined {
-    const result = getExampleNumber(countryISOCode, Examples);
-    return !!result ? result.number.toString() : undefined;
+    const exampleNumber = getExampleNumber(countryISOCode, Examples);
+    return exampleNumber ? exampleNumber.formatNational() : undefined;
   }
 
-  onTouched = () => {
-  };
+  onTouched = () => {};
 
-  propagateChange = (_: any) => {
-  };
+  propagateChange = (_: any) => {};
 
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
@@ -187,19 +183,31 @@ export class NgxMatIntlTelInputComponent
     }
   }
 
+  setupSearch(): void {
+    this.searchCriteriaControl.valueChanges.subscribe((value) => {
+      this.filterCountries(value || "");
+    });
+  }
+
+  searchCriteriaControl = new FormControl("");
+  filteredCountries: Array<Country> = [];
+
+  filterCountries(searchText: string): void {
+    if (!searchText) {
+      this.filteredCountries = this.allCountries;
+    } else {
+      const filterValue = searchText.toLowerCase();
+      this.filteredCountries = this.allCountries.filter(
+        (country) =>
+          country.name.toLowerCase().includes(filterValue) ||
+          country.dialCode.includes(filterValue)
+      );
+    }
+  }
+
   ngOnInit(): void {
     if (!this.searchPlaceholder) {
-      this.searchPlaceholder = 'Search ...';
-    }
-    if (this.preferredCountries.length) {
-      this.preferredCountries.forEach((iso2) => {
-        const preferredCountry = this.allCountries
-          .filter((c) => c.iso2 === iso2)
-          .shift();
-        if (preferredCountry) {
-          this.preferredCountriesInDropDown.push(preferredCountry);
-        }
-      });
+      this.searchPlaceholder = "Search ...";
     }
     if (this.onlyCountries.length) {
       this.allCountries = this.allCountries.filter((c) =>
@@ -210,15 +218,17 @@ export class NgxMatIntlTelInputComponent
       // If an existing number is present, we use it to determine selectedCountry
       this.selectedCountry = this.getCountry(this.numberInstance.country);
     } else {
-      if (this.preferredCountriesInDropDown.length) {
-        this.selectedCountry = this.preferredCountriesInDropDown[0];
-      } else {
-        this.selectedCountry = this.allCountries[0];
-      }
+      this.selectedCountry = this.allCountries[0];
     }
     this.countryChanged.emit(this.selectedCountry);
+    this.inputPlaceholder =
+      NgxMatIntlTelInputComponent.getPhoneNumberPlaceHolder(
+        this.selectedCountry.iso2.toUpperCase() as CC
+      );
     this._changeDetectorRef.markForCheck();
     this.stateChanges.next(undefined);
+    this.filteredCountries = this.allCountries;
+    this.setupSearch();
   }
 
   ngDoCheck(): void {
@@ -230,7 +240,7 @@ export class NgxMatIntlTelInputComponent
   public onPhoneNumberChange(): void {
     try {
       this.numberInstance = parsePhoneNumberFromString(
-        this.phoneNumber?.toString() || '',
+        this.phoneNumber?.toString() || "",
         this.selectedCountry?.iso2.toUpperCase() as CC
       );
       this.formatAsYouTypeIfEnabled();
@@ -262,20 +272,26 @@ export class NgxMatIntlTelInputComponent
     }
     this.selectedCountry = country;
     this.countryChanged.emit(this.selectedCountry);
+    this.inputPlaceholder =
+      NgxMatIntlTelInputComponent.getPhoneNumberPlaceHolder(
+        country.iso2.toUpperCase() as CC
+      );
     this.onPhoneNumberChange();
-    el.focus();
+    setTimeout(() => {
+      el.focus();
+    });
   }
 
   public getCountry(code: string): Country {
     return (
       this.allCountries.find((c) => c.iso2 === code.toLowerCase()) || {
-        name: 'UN',
-        iso2: 'UN',
-        dialCode: '',
+        name: "UN",
+        iso2: "UN",
+        dialCode: "",
         priority: 0,
         areaCodes: undefined,
-        flagClass: 'UN',
-        placeHolder: '',
+        flagClass: "UN",
+        placeHolder: "",
       }
     );
   }
@@ -296,7 +312,7 @@ export class NgxMatIntlTelInputComponent
         priority: +c[3] || 0,
         areaCodes: (c[4] as string[]) || undefined,
         flagClass: c[1].toString().toUpperCase(),
-        placeHolder: '',
+        placeHolder: "",
       };
 
       if (this.enablePlaceholder) {
@@ -305,7 +321,6 @@ export class NgxMatIntlTelInputComponent
             country.iso2.toUpperCase() as CC
           );
       }
-
       this.allCountries.push(country);
     });
   }
@@ -335,12 +350,6 @@ export class NgxMatIntlTelInputComponent
         }
         setTimeout(() => {
           this.selectedCountry = this.getCountry(countryCode);
-          if (
-            this.selectedCountry.dialCode &&
-            !this.preferredCountries.includes(this.selectedCountry.iso2)
-          ) {
-            this.preferredCountriesInDropDown.push(this.selectedCountry);
-          }
           this.countryChanged.emit(this.selectedCountry);
 
           // Initial value is set
@@ -361,14 +370,14 @@ export class NgxMatIntlTelInputComponent
     return !this.phoneNumber;
   }
 
-  @HostBinding('class.ngx-floating')
+  @HostBinding("class.ngx-floating")
   get shouldLabelFloat(): boolean {
     return this.focused || !this.empty;
   }
 
   @Input()
   get placeholder(): string {
-    return this._placeholder || '';
+    return this._placeholder || "";
   }
 
   set placeholder(value: string) {
@@ -397,17 +406,17 @@ export class NgxMatIntlTelInputComponent
   }
 
   setDescribedByIds(ids: string[]) {
-    this.describedBy = ids.join(' ');
+    this.describedBy = ids.join(" ");
   }
 
   onContainerClick(event: MouseEvent): void {
-    if ((event.target as Element).tagName.toLowerCase() !== 'input') {
-      this.elRef.nativeElement.querySelector('input')!.focus();
+    if ((event.target as Element).tagName.toLowerCase() !== "input") {
+      this.elRef.nativeElement.querySelector("input")!.focus();
     }
   }
 
   reset(): void {
-    this.phoneNumber = '';
+    this.phoneNumber = "";
     this.propagateChange(null);
 
     this._changeDetectorRef.markForCheck();
@@ -421,12 +430,12 @@ export class NgxMatIntlTelInputComponent
 
   private get formattedPhoneNumber(): string {
     if (!this.numberInstance) {
-      return this.phoneNumber?.toString() || '';
+      return this.phoneNumber?.toString() || "";
     }
     switch (this.format) {
-      case 'national':
+      case "national":
         return this.numberInstance.formatNational();
-      case 'international':
+      case "international":
         return this.numberInstance.formatInternational();
       default:
         return this.numberInstance.nationalNumber.toString();
@@ -434,7 +443,7 @@ export class NgxMatIntlTelInputComponent
   }
 
   private formatAsYouTypeIfEnabled(): void {
-    if (this.format === 'default') {
+    if (this.format === "default") {
       return;
     }
     const asYouType: AsYouType = new AsYouType(
@@ -444,7 +453,7 @@ export class NgxMatIntlTelInputComponent
     if (
       this.phoneNumber
         ?.toString()
-        .startsWith(this.previousFormattedNumber || '')
+        .startsWith(this.previousFormattedNumber || "")
     ) {
       this.phoneNumber = asYouType.input(this.phoneNumber.toString());
     }
